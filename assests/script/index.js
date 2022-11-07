72777341d04f1aeefad6d71e2712550c913860d9
MrKunalmittal <kunalmittal19498@gmail.com>
2022-11-07 15:15:01 -0500

    Index Created

---
+++ assests/script/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/*
+    java script
+
+   detecting window sixe and cursor coordinates
+
+*/
+
+const pageW=document.querySelector('.page-w');
+const pageH=document.querySelector('.page-h');
+
+const cursorX=document.querySelector('.cursor-x');
+const cursorY=document.querySelector('.cursor-y');
+
+const circle=document.querySelector('.circle');
+
+
+function readWindow() {
+    pageW.innerText =  `window width: ${window.innerWidth}px`;
+    pageH.innerText =  `window height: ${window.innerHeight}px`;
+}
+
+window.addEventListener('resize', () => {
+    readWindow();
+});
+
+window.addEventListener('load', () => {
+    readWindow();
+});
+
+
+
+
+  
